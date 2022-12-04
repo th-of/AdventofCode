4758b99de3d652c8d392cc97be61cdefdf326231
th-of <46278528+th-of@users.noreply.github.com>
2022-12-04 16:12:03 +0100

    Add files via upload

---
+++ AoC2022/Day4/Day4_part2.py
@@ -0,0 +1,12 @@
+def intersection(comp1, comp2):
+    intersect = [value for value in comp1 if value in comp2]
+    return intersect
+
+n = 0
+with open("input.txt") as f:
+    for line in f:
+        a = range(int(line.split(",")[0].split("-")[0]), int(line.split(",")[0].split("-")[1])+1)
+        b = range(int(line.split(",")[1].split("-")[0]), int(line.split(",")[1].split("-")[1])+1)
+        if len(intersection(a, b)) > 0 or len(intersection(a, b)) > 0:
+            n += 1    
+print(n)
